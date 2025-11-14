9d3b27c112f76366e0d63049710531fb5826ac26
EduardhaLima <eduardharflimabr@gmail.com>
2025-11-14 16:05:33 -0300

    operacoes-matematicas-2

--- operacoes-matematicas.js
+++ operacoes-matematicas.js
@@ -1,3 +1,4 @@
+//soma
 console.log(2+2)
 
 const numero1 = 2
@@ -6,3 +7,36 @@ const numero2 = 2
 const resultado = numero1 + numero2 
 
 console.log(resultado)
+
+//subtracao
+console.log(2 - 2)
+
+let num1 = 2
+let num2 = 2
+
+let subtracao = num1 - num2
+
+console.log(subtracao)
+
+//multiplicacao
+console.log(2*2)
+
+const num3 = 2
+const num4 = 2
+
+const multiplicacao = num3 * num4 
+
+console.log(multiplicacao)
+
+//divisao
+console.log(10/2)
+//mais é mesma coisa dos outros
+
+//resto da divisão
+console.log(15%2)
+//mais é a mesma coisa dos outros
+
+//exponenciação
+console.log(2**3)
+//mais é a mesma coisa dos outros
+
