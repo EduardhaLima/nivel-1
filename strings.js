8c26ee8a00f5f3187bb8472ee19150ef202146ef
EduardhaLima <eduardharflimabr@gmail.com>
2025-11-14 16:31:43 -0300

    string-2

--- strings.js
+++ strings.js
@@ -3,9 +3,14 @@ let nome = "Eduardha Lima"
 console.log(nome.split(" ")) //para separar a string
 
 let tamanhoDaString = nome.length //para ver o tamanho da string
-console.log(tamanhoDaStrign)
+console.log(tamanhoDaString)
 
 console.log(nome.charAt(6)) //para mostrar o que está nessa posição na string
 
-console.log(nome.toLowerCase()) //para todas as letras ficarem minusculas
-console.log(nome.toUpperCase()) //para todas as letras ficarem maiusculas
+console.log(nome.toLowerCase(5)) //para todas as letras ficarem minusculas
+console.log(nome.toUpperCase(0)) //para todas as letras ficarem maiusculas
+
+console.log(nome.substring(2,5)) //para selecionar apenas o que esta na posicao dois ate a posicao 5
+
+console.log(nome.slice(-4)) //recebe numeros negativos, ou seja, vai imprimir de tras para frente de acordo com a localizacao que pedimos
+//pode funcionar tambem como uma substring, com a vantagem de ter numeros negativos
