ed748c25036156fae8e1f89ad5d633ab4c4ff5dd
EduardhaLima <eduardharflimabr@gmail.com>
2025-11-14 16:21:14 -0300

    string

---
+++ strings.js
@@ -0,0 +1,11 @@
+let nome = "Eduardha Lima"
+
+console.log(nome.split(" ")) //para separar a string
+
+let tamanhoDaString = nome.length //para ver o tamanho da string
+console.log(tamanhoDaStrign)
+
+console.log(nome.charAt(6)) //para mostrar o que está nessa posição na string
+
+console.log(nome.toLowerCase()) //para todas as letras ficarem minusculas
+console.log(nome.toUpperCase()) //para todas as letras ficarem maiusculas
